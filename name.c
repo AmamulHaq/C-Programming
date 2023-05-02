3bbb6a81370e1119027e822f3ab7dbe7c0cc5161
AmamulHaq <132338103+AmamulHaq@users.noreply.github.com>
2023-05-02 13:22:19 +0545

    added new programms

---
+++ name.c
@@ -0,0 +1,36 @@
+//programe to display my name
+
+#include <stdio.h>
+int main()
+{
+
+char fname[15];
+char lname[15];
+int age;
+printf("What is your first name?\n");
+scanf("%s",fname);
+
+printf("What is your last name?\n");
+scanf("%s",lname);
+
+
+printf("your name is %s %s",fname,lname);
+
+
+printf("\nWhat is  your age ?\n");
+scanf("%d",&age);
+printf("your age is %d ",age);
+
+
+
+
+
+return 0;
+
+
+
+
+
+
+
+}
