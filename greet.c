a620bd9691289b40e29ce2bd0ac968578747a3c7
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-05-21 21:37:07 +0545

    GREETING

--- greet.c
+++ greet.c
@@ -1,2 +1,20 @@
 #include<stdio.h>
 #include<conio.h>
+int main()
+{
+char fname[15];
+char lname[15];
+
+
+printf("What is your first name?\n");
+scanf("%s",fname);                                                                              
+
+printf("What is your last name?\n");
+scanf("%s",lname);
+
+
+printf("HAPPY ANNIVERSARY %s %s\n Have a good day.",fname,lname);
+
+return 0;
+
+}
