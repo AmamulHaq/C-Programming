a4c94e15cb018ae83bc5ecce7ad702ca62ae463f
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-05-22 22:38:08 +0545

    greatest number calculator

--- greater.c
+++ greater.c
@@ -3,20 +3,28 @@
 
 int main()
 {
-int a,b,c;
-printf("Enter any three numbers:");
-scanf("%d,%d,%d",&a,&b,&c);
+    int a, b, c;
+    printf("Enter any three numbers:\n");
+    printf("Enter first number:");
+    scanf("%d", &a);
+    printf("Enter second  number:");
+    scanf("%d", &b);
+    printf("Enter third number:");
+    scanf("%d", &c);
+    if (a > b)
+    {
+        if (a > c)
+            printf("The greater number is: %d", a);
+        else
+            printf("The greater number is: %d", c);
+    }
+    else
+    {
+        if (b > c)
+            printf("The greater number is: %d", b);
+        else
+            printf("The greater number is: %d", c);
+    }
 
-if (a>b)
-{if (a>c)
-printf("The greater number is: %d",a);
-else printf("The greater number is: %d",c);
-}
-else {
-if(b>c)
-printf("The greater number is: %d",b);
-else printf("The greater number is: %d",c);
-}
-
-return 0;
+    return 0;
 }
