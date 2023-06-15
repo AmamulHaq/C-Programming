42f11c7e093daa096248da944b011ad53a5a4c8b
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-06-15 08:36:24 +0545

    types of data inputs

---
+++ datatype.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+
+int myNum = 5;            
+float myFloatNum = 5.99;  
+char myLetter = 'D';
+char myName[24] = "Amamul Haq";    
+
+// Print variables
+printf("%d\n", myNum);
+printf("%.3f\n", myFloatNum);
+printf("%c\n", myLetter);
+printf("%s\n", myName);
+
+}
