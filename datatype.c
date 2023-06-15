6d81830699b4c94df64dc97443ab831d193d6943
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-06-15 16:28:17 +0545

    input data types

--- datatype.c
+++ datatype.c
@@ -4,15 +4,15 @@
 int main()
 {
 
-int myNum = 5;            
-float myFloatNum = 5.99;  
-char myLetter = 'D';
-char myName[24] = "Amamul Haq";    
+int myNum = 5 , herNum = 7;            
+float myFloatNum = 24.4 , herFloatNum = 42.2 ;  
+char myLetter = 'A' , herLetter = 'R' ;
+char myName[24]= "Amamul Haq" , herName[25]= "Rakshya Raut" ;    
 
 // Print variables
-printf("%d\n", myNum);
-printf("%.3f\n", myFloatNum);
-printf("%c\n", myLetter);
-printf("%s\n", myName);
+printf("%d\n%d\n", myNum, herNum);
+printf("%.3f\n%.3f\n", myFloatNum , herFloatNum);
+printf("%c\n%c\n", myLetter , herLetter);
+printf("%s\n%s\n", myName , herName );
 
 }
