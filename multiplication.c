16eabd8443d1432a5d51ecfeb5370a7351d79560
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-07-26 22:43:39 +0545

    addition of two matrixes

--- multiplication.c
+++ multiplication.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
-int main()
-{
-    int a = 20, b = 20, m;
 
-    printf("ENTER 1st NUMBER:");
-    scanf("%d", &a);
+void add_matrices(int rows, int cols, int matrix1[][cols], int matrix2[][cols], int result[][cols]) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+}
 
-    printf("ENTER 2nd NUMBER:");
-    scanf("%d", &b);
+// Example usage:
+int main() {
+    int rows = 3;
+    int cols = 3;
+    int matrix1[3][3] = {{1, 2, 3},
+                         {4, 5, 6},
+                         {7, 8, 9}};
+    int matrix2[3][3] = {{9, 8, 7},
+                         {6, 5, 4},
+                         {3, 2, 1}};
+    int result[3][3];
 
-    m = a * b;
+    add_matrices(rows, cols, matrix1, matrix2, result);
 
-    printf("\n THEIR MULTIPLICATION IS %d", m);
+    // Print the result matrix
+    printf("Result Matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
