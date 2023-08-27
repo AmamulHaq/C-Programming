3231e5559b0f6d65e8fe8295bd208b8bd5f4130f
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-08-28 01:08:25 +0545

    fibonacci series

---
+++ series2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    int n, s1 = 0, s2 = 1, next;
+    printf("Enter no of terms for Fibonacci series :");
+    scanf("%d", &n);
+    printf("First %d terms of Fibonacci are :", n);
+    printf("\t%d\t%d", s1, s2);
+    for (int i = 3; i <=n; i++)
+    {
+        {
+            next = s1 + s2;
+            s1 = s2;
+            s2 = next;
+        }
+
+        printf("\t%d", next);
+    }
+
+    return 0;
+}
