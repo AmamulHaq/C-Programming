c4194e1179e4ff09dd621fa2cdbe0d8d733e85f0
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-08-28 17:43:13 +0545

    Read and define strings

---
+++ series5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+main()
+{
+    char para[1000];
+    printf("Write a paragraph..\n");
+    fgets(para, sizeof(para), stdin);
+    for (int i = 0; para[i] != '\0'; i++)
+    {para[i] = toupper(para[i]);}// define different conditions
+    printf("The entered inputs are:\n%s",para);
+}
