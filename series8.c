485ee6540d23373cb079803e949acd8aaf79084c
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-08-28 20:50:10 +0545

    reverse of paragraph

--- series8.c
+++ series8.c
@@ -3,5 +3,15 @@
 
 int main()
 {
-   
+    int t;
+    char name[1000];
+    printf("what is your name?..\n");
+    fgets(name, sizeof(name), stdin);
+
+    t = strlen(name);
+    printf("\nreverse of your name is:\n");
+    for (int i = t - 1; i >= 0; i--)
+    {
+        printf("%c", name[i]);
+    }
 }
