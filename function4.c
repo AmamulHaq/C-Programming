17183ee75bbba52f3895c754d411d51189f475bd
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-09-07 23:21:44 +0545

    sum of numbers using function

--- function4.c
+++ function4.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-int sum(int a, int b);
+int sum(int n[], int size); // Added 'size' parameter
 int main()
 {
-int a,b,s;
-printf("Enter two numbers to sum : ");
-scanf("%d%d",&a,&b);
-s=sum(a,b);
-printf("\nThe sum of numbers are %d",s);
-return 0;}
-int sum(int a, int b){
-    return a+b;
+    int n[5], s = 0;
+    printf("Enter how many numbers to add: \n");
+    for (int i = 0; i < sizeof(n) / sizeof(n[0]); i++)
+        scanf("%d", &n[i]);
+    s = sum(n, sizeof(n) / sizeof(n[0])); // Pass the size of the array
+    printf("\nThe sum of numbers is %d", s);
+    return 0;
+}
+
+int sum(int n[], int size) // Added 'size' parameter
+{
+    int s = 0;
+    for (int i = 0; i < size; i++) // Removed inner 'int i'
+        s += n[i];
+    return s;
 }
