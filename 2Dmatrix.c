18c35d595a71e3e105daddf45c9029ba0005a738
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-09-23 12:32:30 +0545

    matrix addition and their multiplication

--- 2Dmatrix.c
+++ 2Dmatrix.c
@@ -2,15 +2,35 @@
 
 int main()
 {
-    int m[3][3] = {{1, 4, 2},{3, 6, 8},{12, 3, 8}};
-    int n[3][3] = {{7,8,6},{9,2,0},{1,2,3}};
-    int product[3][3];
-      for (int i = 1; i <=3; i++) for (int k = 1; k <=3; k++) {product[i][k] =1;
-        for (int j = 1; j <=3; j++) product[i][k] += m[i][j] * n[j][k];}
-    
-    printf("\nProduct of the matrices:\n");
-    for (int i = 1; i <=3; i++) {for (int k = 1; k <=3; k++) printf("%d\t", product[i][k]);
-    
-    printf("\n");}
-    return 0;}
+  int m[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  int n[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  int product[3][3];
+  int sum[3][3];
 
+  printf("\nSum of the matrices:\n");
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      sum[i][j] = m[i][j] + n[i][j];
+      printf("%d\t", sum[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\nProduct of the matrices:\n");
+  for (int i = 0; i < 3; i++)
+  {
+    for (int k = 0; k < 3; k++)
+    {
+      product[i][k] = 0;
+      for (int j = 0; j < 3; j++)
+      {
+        product[i][k] += m[i][j] * n[j][k];
+      }
+      printf("%d\t", product[i][k]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
--- matrix.c
+++ matrix.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 main() {printf("\tKNOW CONDITION FOR MATRIX MULTIPLICATION\n");
-    int a,b,c,d,m[10][10],n[10][10],product[10][10];
+    int a,b,c,d,m[10][10],n[10][10],product[10][10],sum[10][10],i,j,k;
     printf("\nEnter order of first matrix m:\n");scanf("%d %d", &a, &b);
     printf("\nEnter order of second matrix n:\n");scanf("%d %d", &c, &d);
    if (b!=c){printf("\n\nSORRY!As columns in matrix m is not equal to rows in matrix n.\n\n"); return 1;}
@@ -11,10 +11,18 @@ main() {printf("\tKNOW CONDITION FOR MATRIX MULTIPLICATION\n");
     for (int j = 1; j <=b; j++) {printf("\nEnter elements of row%d:\n",j);
      for (int k = 1; k <=d; k++) scanf("%d", &n[j][k]);}
      
-      for (int i = 1; i <=a; i++) for (int k = 1; k <=d; k++) {product[i][k] =1;
-        for (int j = 1; j <=b; j++) product[i][k] += m[i][j] * n[j][k];}
-    
     printf("\nProduct of the matrices:\n");
-    for (int i = 1; i <=a; i++) {for (int k = 1; k <=d; k++) printf("%d\t", product[i][k]);
-printf("\n");}}
+     for (int i = 1; i <= a; i++){for (int k = 1; k <= d; k++){product[i][k] = 0;
+      for (int j =1; j <=b ; j++){product[i][k] += m[i][j] * n[j][k];}
+      printf("%d\t", product[i][k]);}printf("\n");}
+   if (a!=d){printf("Their sum is not possible\n");return 1;}
+     printf("\nSum of the matrices:\n");
+      for (int i = 1; i <=a; i++){for (int j =1; j <=d; j++)
+      {sum[i][j] = m[i][j] + n[i][j];
+      printf("%d\t", sum[i][j]);}printf("\n");}
+      
+      }
+      
+  
+
 
