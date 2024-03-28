dcb431c44243d54d2117a47be68416ed8fddbe37
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-03-28 21:56:29 +0545

    compound interest

--- compound.c
+++ compound.c
@@ -1,19 +1,30 @@
+//compound interest working
 #include <stdio.h>
+#include <math.h>
+void compound(float p, int t, int r)
+{
+    float a, c;
+    float rate = r / 100.0;
+
+    a = p * pow(1 + rate, t);
+    c = a - p;
+
+    printf("The compound interest amount is %f\n", c);
+    printf("The net amount is %f\n", a);
+}
+
 int main()
 {
-    int p, t, r, c, a;
-    printf("Enter principle amount :");
-    scanf("%d", p);
+    int t, r;
+    float p;
+    printf("Enter principle amount: ");
+    scanf("%f", &p);
 
-    printf("Enter time ratio in year :");
-    scanf("%d", t);
+    printf("Enter time ratio in years: ");
+    scanf("%d", &t);
 
-    printf("Enter rate of Interest :");
-    scanf("%d", r);
-
-    c = p * (1 + r / 100) ^ (t)-p;
-    a = p * (1 + r / 100) ^ (t);
-
-    printf("the compound interest amount is %d\n", c);
-    printf("the net amount is %d", a);
+    printf("Enter rate of Interest: ");
+    scanf("%d", &r);
+    compound(p, t, r);
+    return 0;
 }
