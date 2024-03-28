97b288be2e46e257e973cc821045ebd1b41af22a
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-03-28 21:15:38 +0545

    multiplication

--- multipletable.c
+++ multipletable.c
@@ -1,18 +1,16 @@
-#include<stdio.h>
-int main(){
-int i,n;
-printf ("Enter any integer:");
-scanf("%d",&n);
-printf("Multiplication Table of %d is\n",n);
+//multiplication table
+#include <stdio.h>
+int main()
+{
+    int i, n;
+    printf("Enter any integer:");
+    scanf("%d", &n);
+    printf("Multiplication Table of %d is\n", n);
 
-for(i =1;i<=10;i++){
-printf("%d*%d=%d\n",n,i,n*i);
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d*%d=%d\n", n, i, n * i);
+    }
 
-
-}
-
-return 0;
-
-
-
+    return 0;
 }
