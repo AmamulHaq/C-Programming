b7ccc7281ed4a734b988195d8600b4529ce455ce
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2024-04-07 22:33:20 +0545

    fibonachi sequence

--- series1.c
+++ series1.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-float sum,r;
+int main()
+{
+  float sum, r;
 
-printf("1 + 2x + 3x^2 + 4x^3 + ...\n");
+  printf("1 + 2x + 3x^2 + 4x^3 + ...\n");
 
-printf("\nEnter the ratio r <1 of series:");
-scanf("%f",&r);
- sum = pow((1 - r), -2);
-   printf("\nThe sum of the series up to infinity is %f\n", sum);
-    return 0;
+  printf("\nEnter the ratio r <1 of series:");
+  scanf("%f", &r);
+  sum = pow((1 - r), -2);
+  printf("\nThe sum of the series up to infinity is %f\n", sum);
+  return 0;
 }
--- series2.c
+++ series2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <math.h>
-
+//fibonacci sequence
 int main()
 {
 
     int n, s1 = 0, s2 = 1, next;
-    printf("Enter no of terms for Fibonacci series :");
+    printf("Enter no of terms for Fibonacci sequence :");
     scanf("%d", &n);
     printf("First %d terms of Fibonacci are :", n);
     printf("\t%d\t%d", s1, s2);
