99c9716c4bc65ae9813fd5ddfa826173f7371e8b
AmamulHaq <mdamamul78692@gmail.com>
2024-04-13 16:52:35 +0545

    corrected sinx and cos x series

--- cosx.c
+++ cosx.c
@@ -1,21 +1,32 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
+
+int fact(int n) {
+    if(n == 0 || n == 1) {
+        return 1;
+    }
+    int fact = 1;
+    for(int i = n; i > 1; i--) {
+        fact *= i;
+    }
+    return fact;
+}
 
 int main() {
-    float x, s = 0, t, nume, dino;
-    int i;
-    printf("Enter any value for x in radians:");
-    scanf("%f", &x);
-
-    for (i = 0; i < 6; i++) {
-        nume = pow(-1, i) * pow(x, 2 * i);
-        dino = 1;
-        for (int j = 1; j <= 2 * i; j++) {
-            dino *= j;
-        }
-        t = nume / dino;
-        s = s + t;
+    int n = 3;
+    double x, t, sum = 0;
+    
+    printf("Enter degree: ");
+    scanf("%lf", &x);
+    
+    x = x * 3.1415 / 180;
+    
+    for(int i = 1; i <= n; i++) {
+        t = 2 * i - 2;
+        sum += pow(-1, i + 1) * pow(x, t) / fact(t);
     }
-    printf("cos(%f) = %f", x, s);
+    
+    printf("cos(%.2f) = %.3f", x*180/3.1415, sum);
+    
     return 0;
 }
